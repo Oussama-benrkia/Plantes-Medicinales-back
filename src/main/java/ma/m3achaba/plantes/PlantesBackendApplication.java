114613a25651105f6075c3c16d21ce40aaee468b
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-12-02 23:53:14 +0100

    set up project

---
+++ src/main/java/ma/m3achaba/plantes/PlantesBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.m3achaba.plantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/m3achaba/plantes/PlantesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.m3achaba.plantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
