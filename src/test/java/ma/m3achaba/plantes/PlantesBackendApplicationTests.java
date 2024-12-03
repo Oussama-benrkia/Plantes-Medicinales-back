b18f60813ff9df902cda2c874f15d18e5732eefa
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-12-03 10:14:35 +0100

    fixed sonar and test

--- src/test/java/ma/m3achaba/plantes/PlantesBackendApplicationTests.java
+++ src/test/java/ma/m3achaba/plantes/PlantesBackendApplicationTests.java
@@ -1,13 +1,16 @@
 package ma.m3achaba.plantes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class PlantesBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info( "Hello World!" );
     }
 
 }
